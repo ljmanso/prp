c5451a5b92355ac94b67c08e8013a249c82992fa
Luis J. Manso <luis.manso@gmail.com>
2016-03-18 12:13:34 +0100

    make agmmission ask for model when initializing

--- components/objectOracle/src/specificworker.cpp
+++ components/objectOracle/src/specificworker.cpp
@@ -74,9 +74,17 @@ bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
 {
 	QMutexLocker locker(mutex);
 	
-	RoboCompAGMWorldModel::World w = agmexecutive_proxy->getModel();
-	structuralChange(w);
-
+	try
+	{
+		RoboCompAGMWorldModel::World w = agmexecutive_proxy->getModel();
+		structuralChange(w);
+	}
+	catch(...)
+	{
+		printf("The executive is probably not running, waiting for first AGM model publication...");
+	}
+	
+	
 	timer.start(Period);
 	return true;
 }
