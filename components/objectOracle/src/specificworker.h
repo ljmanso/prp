28b98cca854274d854f3b8905566632a3250d70b
Luis J. Manso <luis.manso@gmail.com>
2016-08-25 14:27:32 +0200

    opencv compiling issues fixed

--- components/objectOracle/src/specificworker.h
+++ components/objectOracle/src/specificworker.h
@@ -63,7 +63,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/utility.hpp>
-#include <opencv2/saliency.hpp>
+//#include <opencv2/saliency.hpp>
 #include <opencv2/highgui.hpp>
 #include "opencv2/imgcodecs.hpp"
 
